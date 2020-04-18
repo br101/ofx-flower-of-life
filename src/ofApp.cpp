d95d840565b09f8592e4de413b0f19b86a805279
Bruno Randolf <br1@einfach.org>
2020-04-18 22:05:44 +0100

    logging/comments

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -48,10 +48,10 @@ void ofApp::generate_flower_of_life(int rounds, int center_x, int center_y, int 
     petals.push_back(Petal(center_x, center_y, radius, 0, 0));
     for (int r = 1; r < rounds; r++) {
         for (int i = 0; i < r * 6; i++) {
-            ofLog(OF_LOG_NOTICE) << "round " << r << " i " << i;
+            ofLog(OF_LOG_NOTICE, "round %d num %d i %d", r, num, i);
             if (r == 1 && i == 0) {
                 /* second circle (number 1, round 1)
-                 *defines the angle of it all */
+                 * defines the angle of it all */
                 int xoff = glm::cos(glm::radians((float)angle)) * radius;
                 int yoff = glm::sin(glm::radians((float)angle)) * radius;
                 ofLog(OF_LOG_NOTICE, "xoff %d yoff %d ang %d", xoff, yoff, angle);
@@ -59,21 +59,19 @@ void ofApp::generate_flower_of_life(int rounds, int center_x, int center_y, int 
             } else {
                 /* all other circles: intersection of the previous circle
                  * with the circle to intersect with */
-                ofLog(OF_LOG_NOTICE, "intersect %d with %d", num-1, intersectWith);
                 Petal& p = petals[num - 1];
-                auto is = p.intersect(petals[intersectWith]);
-                ofLog(OF_LOG_NOTICE, "intersect %d/%d with %d/%d", p.x, p.y,
-                        petals[intersectWith].x, petals[intersectWith].y);
-
+                Petal& p2 = petals[intersectWith];
+                ofLog(OF_LOG_NOTICE, "intersect %d (%d/%d) with %d (%d/%d)",
+                    num-1, p.x, p.y, intersectWith, p2.x, p2.y);
+                auto is = p.intersect(p2);
                 for (auto i : is) {
                     ofLog(OF_LOG_NOTICE, "  is %f %f", i[0], i[1]);
                 }
-
                 petals.push_back(Petal(is[0], radius, num, r));
             }
             num++;
 
-            //finding the circle to make the next intersection with
+            /* finding the circle to make the next intersection with */
             if (i == r * 6 - 1) { // last circle of round
                 intersectWith = intersectWith + 1;
             } else if (i % r) { // not on 60 degrees angle
